1f0da35cd5f11e20448d4e3e68e79b970f7886b4
Arief Maulana <ariefmaulana1303@gmail.com>
2023-08-16 18:36:27 +0800

    Perbaharui eslint

--- backend/.eslintrc.js
+++ backend/.eslintrc.js
@@ -1,15 +1,23 @@
 module.exports = {
-    "env": {
-        "commonjs": true,
-        "es2021": true,
-        "node": true
-    },
-    "extends": "eslint:recommended",
-    "overrides": [
-    ],
-    "parserOptions": {
-        "ecmaVersion": "latest"
-    },
-    "rules": {
+  "env": {
+    "jest/globals": true,
+    "commonjs": true,
+    "es2021": true,
+    "node": true
+  },
+  "extends": "eslint:recommended",
+  "overrides": [
+    {
+      "files": ["__test__/**"],
+      "plugins": ["jest"],
+      "extends": ["plugin:jest/recommended"],
+      "rules": { "jest/prefer-expect-assertions": "off" }
     }
+  ],
+  "parserOptions": {
+    "ecmaVersion": "latest"
+  },
+  "rules": {
+    "indent" : ["error", 2]
+  },
 }
