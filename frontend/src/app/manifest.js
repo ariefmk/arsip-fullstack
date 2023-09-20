b4ad0039c23cb2a29357570ee26b41bb4ea7e2a2
Arief Maulana <ariefmaulana1303@gmail.com>
2023-09-20 21:47:18 +0800

    feat(frontend|manifest): Mengatur manifest untuk aplikasi web
    - Membuat berkas manifest.js pada folder app
    - Menambahkan icon-512 pada folder public
    - Mengatur konfigurasi manifest.js

---
+++ frontend/src/app/manifest.js
@@ -0,0 +1,17 @@
+export default function manifest() {
+  return {
+    name: 'E-Arsip Desa Mampari',
+    short_name: 'E-Arsip Desa Mampari',
+    description: 'Aplikasi Manajemen Arsip Kantor Desa Mampari',
+    start_url: '/',
+    display: 'standalone',
+    icons: [
+      {
+        src: 'icon/icon-512.png',
+        sizes: '512x512',
+        type: 'image/png'
+      }
+    ]
+  }
+}
+
