91ff94963226c8acfa0bcbae3322264253999259
Arief Maulana <ariefmaulana1303@gmail.com>
2023-10-14 02:31:01 +0800

    feat(halaman pengguna): Mengatur halaman manajemn pengguna
    - Membuat komponen header untuk menambahkan data dan melakukan pencarian
      data (belum berfungsi)
    - Membuat komponen tabel untuk menampilkan daftar pengguna
    - Membuat datalist untuk diteruskan ke tabel

---
+++ frontend/src/app/(auth)/manajemen/pengguna/components/header.jsx
@@ -0,0 +1,22 @@
+import { IconCirclePlus } from '@tabler/icons-react'
+export default function Header() {
+  return (
+    <div className='flex h-[4rem] w-full flex-row items-center justify-between px-[.5rem] md:px-[2rem]'>
+      <button className='flex h-[2.5rem] items-center justify-center gap-x-1 rounded-full border-2 border-black md:w-[8rem]'>
+        <span>Tambah</span>
+        <IconCirclePlus
+          className='h-[25px] w-[25px]'
+          color='#000000'
+          stroke={2}
+        />
+      </button>
+      <div>
+        <input
+          className='border=2 h-[2.5rem] w-[7rem] rounded-full border-2 border-black px-[15px] outline-none md:w-[20rem]'
+          type='text'
+          placeholder='Pencarian'
+        />
+      </div>
+    </div>
+  )
+}
--- frontend/src/app/(auth)/manajemen/pengguna/page.jsx
+++ frontend/src/app/(auth)/manajemen/pengguna/page.jsx
@@ -1,51 +1,85 @@
-'use client'
-import { useState, useEffect } from 'react'
-import { IconCirclePlus, IconCircleMinus, IconEdit, IconSearch } from '@tabler/icons-react'
+import {
+  IconCirclePlus,
+  IconCircleMinus,
+  IconEdit,
+  IconSearch,
+} from '@tabler/icons-react'
+import Header from './components/header'
+import Tabel from './components/tabel'
 
 export default function Pengguna() {
-
+  const datalist = [
+    {
+      hak: 'Pengguna',
+      nik: '6311041302010004',
+      nama: 'Arief Maulana',
+      bidang: '-',
+      jabatan: '-',
+      tanggal: '13 Februari 2001',
+      kelamin: 'Laki-Laki',
+      telepon: '083125902067',
+      alamat: 'Sumpung, Desa Mampari RT 4 dasgs fsefs fsf',
+    },
+    {
+      hak: 'Pengguna',
+      nik: '6311041302010002',
+      nama: 'Arief Maulana',
+      bidang: '-',
+      jabatan: '-',
+      tanggal: '13 Februari 2001',
+      kelamin: 'Laki-Laki',
+      telepon: '083125902067',
+      alamat: 'Sumpung, Desa Mampari RT 4 dasgs fsefs fsf',
+    },
+    {
+      hak: 'Pengguna',
+      nik: '6311041302010011',
+      nama: 'Arief Maulana',
+      bidang: '-',
+      jabatan: '-',
+      tanggal: '13 Februari 2022',
+      kelamin: 'Laki-Laki',
+      telepon: '083125902067',
+      alamat: 'Sumpung, Desa Mapari sefs fsf',
+    },
+  ]
   return (
     <div>
-      <div className='px-[.5rem] md:px-[2rem] w-full h-[4rem] bg-lime-500 flex flex-row items-center justify-between'>
+      <Header />
+      {/*
+      <div className='flex h-[4rem] w-full flex-row items-center justify-between bg-lime-500 px-[.5rem] md:px-[2rem]'>
         <div>
-          <button
-            className='flex flex-row gap-x-1 border-2 md:w-[8rem] h-[2.5rem] items-center rounded-full p-[5px] md:py-[5px] md:px-[15px]'
-          >
-            <p className='hidden md:block'>
-              Tambah
-            </p>
+          <button className='flex h-[2.5rem] flex-row items-center gap-x-1 rounded-full border-2 p-[5px] md:w-[8rem] md:px-[15px] md:py-[5px]'>
+            <p className='hidden md:block'>Tambah</p>
             <IconCirclePlus
-              className='w-[25px] h-[25px]'
+              className='h-[25px] w-[25px]'
               color='#000000'
               stroke={2}
             />
           </button>
         </div>
         <div className='flex flex-row gap-x-5'>
-          <div
-          >
-            <select
-              className='border-2 rounded-full w-[6rem] md:w-[14rem] h-[2.5rem] px-[15px]'
-            >
+          <div>
+            <select className='h-[2.5rem] w-[6rem] rounded-full border-2 px-[15px] md:w-[14rem]'>
               <option value=''>Semua</option>
               <option value='admin'>Admin</option>
               <option value='pengguna'>Pengguna</option>
             </select>
           </div>
           <div>
-            <input 
-              className='outline-none border-2 rounded-full w-[7rem] sm:w-[10rem] md:w-[20rem] h-[2.5rem] py-[5px] px-[15px]'
-              type='text' 
+            <input
+              className='h-[2.5rem] w-[7rem] rounded-full border-2 px-[15px] py-[5px] outline-none sm:w-[10rem] md:w-[20rem]'
+              type='text'
               placeholder='Pencarian'
             />
           </div>
         </div>
       </div>
       <div className='w-full overflow-x-auto'>
-        <table className='table-fixed text-center w-full'>
-          <thead className='border-2 border-black h-[3rem]'>
+        <table className='w-full table-fixed text-center'>
+          <thead className='h-[3rem] border-2 border-black'>
             <tr className=''>
-              <th className='w-[50px] '>No</th>
+              <th className='w-[50px]'>No</th>
               <th className='w-[90px]'>Hak Akses</th>
               <th className='w-[160px]'>NIK</th>
               <th className='w-[200px]'>Nama</th>
@@ -55,11 +89,13 @@ export default function Pengguna() {
               <th className='w-[120px]'>Jenis Kelamin</th>
               <th className='w-[180px]'>Nomor Telepon</th>
               <th className='w-[160px] text-ellipsis'>Alamat</th>
-              <th className='w-[160px]' colSpan='2'>Aksi</th>
+              <th className='w-[160px]' colSpan='2'>
+                Aksi
+              </th>
             </tr>
           </thead>
           <tbody>
-            <tr className='odd:bg-lime-200 even:bg-sky-200 h-[2.5rem]'>
+            <tr className='h-[2.5rem] odd:bg-lime-200 even:bg-sky-200'>
               <td>1</td>
               <td>Pengguna</td>
               <td>6311041302010001</td>
@@ -69,33 +105,31 @@ export default function Pengguna() {
               <td>13 Februari 2021</td>
               <td>Laki-Laki</td>
               <td>081234567890</td>
-              <td className='truncate hover:whitespace-normal'>Sumpung, Desa Mampari RT 4 dasgs fsefs fsf</td>
+              <td className='truncate hover:whitespace-normal'>
+                Sumpung, Desa Mampari RT 4 dasgs fsefs fsf
+              </td>
               <td className='w-[80px]'>
-                <button className='flex flex-row gap-x-2 items-center justify-center w-full rounded-full bg-sky-200'>
-                  <p>
-                    Ubah
-                  </p>
+                <button className='flex w-full flex-row items-center justify-center gap-x-2 rounded-full bg-sky-200'>
+                  <p>Ubah</p>
                   <IconEdit
-                    className='w-[20px] h-[20px]'
+                    className='h-[20px] w-[20px]'
                     color='#000000'
                     stroke={2}
                   />
                 </button>
               </td>
               <td className=' w-[80px]'>
-                <button className='flex flex-row gap-x-2 items-center justify-center w-full rounded-full bg-sky-200'>
-                  <p>
-                    Hapus
-                  </p>
+                <button className='flex w-full flex-row items-center justify-center gap-x-2 rounded-full bg-sky-200'>
+                  <p>Hapus</p>
                   <IconCircleMinus
-                    className='w-[20px] h-[20px]'
+                    className='h-[20px] w-[20px]'
                     color='#000000'
                     stroke={2}
                   />
                 </button>
               </td>
             </tr>
-            <tr className='odd:bg-lime-200 even:bg-sky-200 h-[2.5rem]'>
+            <tr className='h-[2.5rem] odd:bg-lime-200 even:bg-sky-200'>
               <td>1</td>
               <td>Admin</td>
               <td>6311041302010001</td>
@@ -105,26 +139,24 @@ export default function Pengguna() {
               <td>13 Februari 2021</td>
               <td>Laki-Laki</td>
               <td>081234567890</td>
-              <td className='truncate hover:whitespace-normal'>Sumpung, Desa Mampari RT 4</td>
+              <td className='truncate hover:whitespace-normal'>
+                Sumpung, Desa Mampari RT 4
+              </td>
               <td className=''>
-                <button className='flex flex-row gap-x-2 rounded-1 rounded-black w-[80px]'>
-                  <p>
-                    Ubah
-                  </p>
+                <button className='rounded-1 rounded-black flex w-[80px] flex-row gap-x-2'>
+                  <p>Ubah</p>
                   <IconEdit
-                    className='w-[20px] h-[20px]'
+                    className='h-[20px] w-[20px]'
                     color='#000000'
                     stroke={2}
                   />
                 </button>
               </td>
               <td>
-                <button className='flex flex-row gap-x-2 rounded-1 rounded-black w-[80px]'>
-                  <p>
-                    Hapus
-                  </p>
+                <button className='rounded-1 rounded-black flex w-[80px] flex-row gap-x-2'>
+                  <p>Hapus</p>
                   <IconCircleMinus
-                    className='w-[20px] h-[20px]'
+                    className='h-[20px] w-[20px]'
                     color='#000000'
                     stroke={2}
                   />
@@ -134,6 +166,8 @@ export default function Pengguna() {
           </tbody>
         </table>
       </div>
+      */}
+      <Tabel datalist={datalist} />
     </div>
   )
 }
